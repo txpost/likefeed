867ab093029db5df868ba1a685209914e18df947
Trevor Postma <txpost@hotmail.com>
2014-12-20 00:11:48 -0400

    add express

--- bot.js
+++ bot.js
@@ -27,49 +27,49 @@ app.get('/db', function (request, response) {
 	})
 })
 
-getTweet = function (cb) {
-	t.get('favorites/list', {user_id: "trevpost"}, function (err, data, response) {
-		if (!err) {
-			// console.log(data);
-			var botData = {
-				baseTweet: data[5].text,
-				tweetID: data[5].id_str,
-				tweetUsername: data[5].user.screen_name
-			};
-			console.log("Tweet: " + botData.baseTweet);
-			// cb(null, botData);
-		} else {
-			console.log("There was an error getting a public Tweet. ABORT!");
-			cb(err, botData);
-		}
-	});
-}
+// getTweet = function (cb) {
+// 	t.get('favorites/list', {user_id: "trevpost"}, function (err, data, response) {
+// 		if (!err) {
+// 			// console.log(data);
+// 			var botData = {
+// 				baseTweet: data[5].text,
+// 				tweetID: data[5].id_str,
+// 				tweetUsername: data[5].user.screen_name
+// 			};
+// 			console.log("Tweet: " + botData.baseTweet);
+// 			// cb(null, botData);
+// 		} else {
+// 			console.log("There was an error getting a public Tweet. ABORT!");
+// 			cb(err, botData);
+// 		}
+// 	});
+// }
 
-formatPost = function (botData, cb) {
-	// body...
-}
+// formatPost = function (botData, cb) {
+// 	// body...
+// }
 
-sendPost = function (botData, cb) {
-	// body...
-}
+// sendPost = function (botData, cb) {
+// 	// body...
+// }
 
 
-// run each function in sequence
-run = function () {
-	async.waterfall([
-		getTweet,
-		formatPost,
-		sendPost
-	],
-	function (err, botData) {
-		if (err) {
-			console.log("There was an error posting to Tumblr: ", err);
-		} else {
-			console.log("Post successful!");
-			console.log("Tweet: ", botData.tweetBlock);
-		}
-	});
-}
+// // run each function in sequence
+// run = function () {
+// 	async.waterfall([
+// 		getTweet,
+// 		formatPost,
+// 		sendPost
+// 	],
+// 	function (err, botData) {
+// 		if (err) {
+// 			console.log("There was an error posting to Tumblr: ", err);
+// 		} else {
+// 			console.log("Post successful!");
+// 			console.log("Tweet: ", botData.tweetBlock);
+// 		}
+// 	});
+// }
 
 
 // run every two hours: 60000 * 60 * 2
