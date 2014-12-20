2a738b7c9b25767f3b19decc54056e86060618fd
Trevor Postma <txpost@hotmail.com>
2014-12-19 23:07:08 -0400

    setup db

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ var t = new Twit({
 	access_token_secret: process.env.TWIT_ACCESS_TOKEN_SECRET
 });
 
-app.get('/db', function (request, response) {
+t.get('/db', function (request, response) {
 	pg.connect(process.env.DATABASE_URL, function  (err, client, done) {
 		client.query('SELECT * FROM test_table', function (err, result) {
 			done();
