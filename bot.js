7de73bab18e16521a8ec967b61149124ee12fad1
Trevor Postma <txpost@hotmail.com>
2014-12-19 20:58:47 -0400

    first deploy

---
+++ bot.js
@@ -0,0 +1,65 @@
+var Twit = require('twit'),
+	async = require('async'),
+	request = require('request');
+
+// authentication for the Twitter API
+var t = new Twit({
+	consumer_key: process.env.TWIT_CONSUMER_KEY,
+	consumer_secret: process.env.TWIT_CONSUMER_SECRET,
+	access_token: process.env.TWIT_ACCESS_TOKEN,
+	access_token_secret: process.env.TWIT_ACCESS_TOKEN_SECRET
+});
+
+getTweet = function (cb) {
+	t.get('favorites/list', {user_id: "trevpost"}, function (err, data, response) {
+		if (!err) {
+			var botData = {
+				baseTweet: data.statuses[0].text,
+				tweetID: data.statuses[0].id_str,
+				tweetUsername: data.statuses[0].user.screen_name
+			};
+			console.log("Tweet: " + botData.baseTweet);
+			// cb(null, botData);
+		} else {
+			console.log("There was an error getting a public Tweet. ABORT!");
+			cb(err, botData);
+		}
+	});
+}
+
+formatPost function (botData, cb) {
+	// body...
+}
+
+sendPost function (botData, cb) {
+	// body...
+}
+
+
+// run each function in sequence
+run = function () {
+	async.waterfall([
+		getTweet,
+		formatPost,
+		sendPost
+	],
+	function (err, botData) {
+		if (err) {
+			console.log("There was an error posting to Tumblr: ", err);
+		} else {
+			console.log("Post successful!");
+			console.log("Tweet: ", botData.tweetBlock);
+		}
+	});
+}
+
+
+// run every two hours: 60000 * 60 * 2
+setInterval(function () {
+	try {
+		run();
+	}
+	catch (e) {
+		console.log(e);
+	}
+}, 60000 * 1);
